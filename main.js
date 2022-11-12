f4328d82ee2ac74b7385eb7ce5a8637dd0c89481
arshsiddiqui <arshsiddiqui@gmail.com>
2022-11-12 10:42:57 -0500

    Randomly generated data established

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-JSC.Chart('graph', {});
+// Generation of random, but negatively sloped data
+const data = [];
+
+let max = 10000;
+for (let i = 0; i < 100; i++) {
+    data[i] = Math.floor(Math.random() * max);
+    max -= 99;
+    console.log(max);
+}
+console.log(data);
+
+JSC.Chart('graph', {
+    type: 'horizontal column',
+    series: [
+        {
+            points: [
+                {x: 'Apples', y: 50},
+                {x: 'Oranges', y: 42}
+            ]
+        }
+    ]
+});
