8eaaac681df75fe0b6f9c64bb313bf996da53801
arshsiddiqui <arshsiddiqui@gmail.com>
2022-11-12 11:30:05 -0500

    Combined data and new graph version

--- main.js
+++ main.js
@@ -5,23 +5,25 @@ const dates = [];
 let max = 10000;
 let idx = 0;
 for (let i = 0; i < 100; i++) {
-    data[idx] = i;
-    idx++;
     data[idx] = Math.floor(Math.random() * max);
     idx++;
     max -= 99;
 
     dates[i] = i;
 }
-console.log(data);
-console.log(dates);
 
-JSC.Chart('graph', {
-    series: [
-        {
-            points: [
-                data
-            ]
-        }
-    ]
+var combinedData = [];
+for (let i = 0; i < 100; i++) {
+    combinedData[i] = {x:dates[i], y:data[i]};
+}
+console.log(combinedData);
+
+var chart = new Chart("graph", {
+    type: "line",
+    data: {
+        datasets: [{
+            data: data
+        }]
+    },
+    options: {}
 });
